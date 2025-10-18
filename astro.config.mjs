f95200be239dfa6ac4eeb5d32a1467c2bf3cd1ed
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2025-10-18 14:32:40 -0600

    WIP site setup & CSS experiments

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Labs 🧪',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'CSS 🎨',
+					items: [
+						{ label: 'Functions', slug: 'css/functions' }
+					]
+				},
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
